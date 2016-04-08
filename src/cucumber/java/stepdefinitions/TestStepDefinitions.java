07615d4da36fb7ff6339619027b6106b2156fdc9
Hongkai Liu <hongkailiu@users.noreply.github.com>
2016-04-08 10:46:14 -0400

    impl. 1st BDD: verify replica set status

--- src/cucumber/java/stepdefinitions/TestStepDefinitions.java
+++ src/cucumber/java/stepdefinitions/TestStepDefinitions.java
@@ -1,6 +1,8 @@
 package stepdefinitions;
 
 
+import com.ericsson.itte.mongoconnectortest.Params;
+import com.ericsson.itte.mongoconnectortest.util.MongoUtil;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -14,16 +16,16 @@ import lombok.extern.slf4j.Slf4j;
 
     @Given("^replica set on the mongo server has been initiated$")
     public void replica_set_on_the_mongo_server_has_been_initiated() throws Throwable {
-        log.info("TODO");
+        // the assumption here is that
+        // the rs.initiate() command is executed already on the server
     }
 
     @When("^I check the configuration on the mongo server$")
     public void I_check_the_configuration_on_the_mongo_server() throws Throwable {
-        log.info("TODO");
     }
 
     @Then("^I see right replica set configuration$")
     public void I_see_right_replica_set_configuration() throws Throwable {
-        log.info("TODO");
+        MongoUtil.checkReplicaSet(Params.MONGO_IP, Params.MONGO_HTTP_PORT, Params.MONGO_HOSTNAME, Params.MONGO_PORT);
     }
 }
---
+++ src/main/java/com/ericsson/itte/mongoconnectortest/Params.java
@@ -0,0 +1,22 @@
+package com.ericsson.itte.mongoconnectortest;
+
+/**
+ * Useful constants
+ */
+public class Params {
+    // "142.133.111.170" "127.0.0.1"
+    public static final String MONGO_IP = "142.133.111.170";
+    public static final int MONGO_HTTP_PORT = 28017;
+    public static final String MONGO_HOSTNAME = "my-mongo";
+    public static final int MONGO_PORT = 27017;
+
+    public static final String COLON = ":";
+    public static final String SLASH = "/";
+    public static final String HTTP = "http" + COLON + SLASH + SLASH;
+
+    public static final String TEST_MONGO = "testmongo";
+    public static final String PERSON = "person";
+    public static final String TEST_MONGO_PERSON_PATH = SLASH + TEST_MONGO + SLASH + PERSON + SLASH;
+
+    public static final String MIME_TYPE_TEXT_PLAIN = "text/plain";
+}
